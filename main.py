2b0ef9b1047ce117f49bc2ad96a2bc87c3f2ac69
Ethan Roundy <ethanroundycode@gmail.com>
2022-06-23 14:16:12 -0600

    Output to console instead of image.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import keras_ocr
+import itertools
 
 
 
@@ -10,17 +11,25 @@ pipeline = keras_ocr.pipeline.Pipeline()
 # Get a set of three example images, needs to be iterable
 images = [
     keras_ocr.tools.read(url) for url in [
-        'images/i4.png', 'images/i5.png'
+        'images/i3.png', 'images/i4.png'
     ]
 ]
 
 # Each list of predictions in prediction_groups is a list of
 # (word, box) tuples.
 prediction_groups = pipeline.recognize(images)
+total_findings = list(itertools.chain(*prediction_groups))
 
+print(total_findings)
+all_words = []
+for pred in total_findings:
+        print(pred[0])
+        all_words.append(pred[0])
+
+print(''.join(all_words).upper())
 # Plot the predictions
-fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
-for ax, image, predictions in zip(axs, images, prediction_groups):
-    keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+# fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
+# for ax, image, predictions in zip(axs, images, prediction_groups):
+#     keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
 
-plt.show()
+# plt.show()
